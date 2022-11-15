50dd3edf961aa5899b9672f17488b8f0d42cdd69
Administrator <2804233156@qq.com>
2022-11-15 13:53:56 +0800

    更新

--- net_learn/readme.md
+++
@@ -1,5 +0,0 @@
-# GoogLeNet学习
-
-#### 1.结构
-
-inception结构是goog_lenet的特殊之处，为了追求对于大规模识别的需求而诞生，
--- net_learn/utils/use_transform.py
+++
@@ -1,7 +0,0 @@
-from torchvision import transforms
-import cv2
-import torch
-
-
-if __name__ == "__main__":
-    print(torch.manual_seed(17))
